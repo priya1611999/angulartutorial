2cbbef83176cd2404e317c7ef66a16763b29711e
priya chaudhari <65346335+priya16chaudhari@users.noreply.github.com>
2025-10-10 01:02:46 +0530

    todays code

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -3,13 +3,14 @@ import { LoginComponent } from './login/login.component';
 import { customecomponent } from './customcomponent/custom.component';
 import { CalculatorComponent } from './calculator/calculator.component';
 import { MouseeventComponent } from './mouseevent/mouseevent.component';
-import { SetgetvalueComponent } from './setgetvalue/setgetvalue.component';
-import { IfelseconditionsComponent } from './ifelseconditions/ifelseconditions.component';
+import { ForloopcodeComponent } from './forloopcode/forloopcode.component';
+import { SignalcodeComponent } from './signalcode/signalcode.component';
 
 @Component({
   selector: 'app-root',
  // imports: [LoginComponent,customecomponent,CalculatorComponent,MouseeventComponent,SetgetvalueComponent,IfelseconditionsComponent],
- imports:[IfelseconditionsComponent],
+//  imports:[IfelseconditionsComponent],
+imports:[ForloopcodeComponent,SignalcodeComponent],
  templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- my-app/src/app/forloopcode/forloopcode.component.ts
+++ my-app/src/app/forloopcode/forloopcode.component.ts
@@ -7,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './forloopcode.component.css'
 })
 export class ForloopcodeComponent {
+userList=['Anil','Sunil','Kiran','Ravi','Ramesh'];
 
+studentList=[{name:'John', age:20}, {name:'Jane', age:22}, {name:'Jim', age:21}];
+getName(name:string){
+  console.log(name)
+}
 }
