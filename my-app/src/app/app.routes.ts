4e8708af5d7d6582b70d48f504ba2f5eb2732e1c
priya chaudhari <65346335+priya16chaudhari@users.noreply.github.com>
2025-10-13 21:57:25 +0530

    get api code

--- my-app/src/app/app.routes.ts
+++ my-app/src/app/app.routes.ts
@@ -1,14 +1,4 @@
 import { Routes } from '@angular/router';
-import { RouteaboutchcomponetComponent } from './routeaboutchcomponet/routeaboutchcomponet.component';
-import { RoutecontactcomponetComponent } from './routecontactcomponet/routecontactcomponet.component';
-import { RoutelogincomponetComponent } from './routelogincomponet/routelogincomponet.component';
-import { RoutehomecomponetComponent } from './routehomecomponet/routehomecomponet.component';
-import { RoutepagenotfoundcomponetComponent } from './routepagenotfoundcomponet/routepagenotfoundcomponet.component';
-import { RouteprofilepassdatacomponetComponent } from './routeprofilepassdatacomponet/routeprofilepassdatacomponet.component';
-import { DynamicroutingcomponetComponent } from './dynamicroutingcomponet/dynamicroutingcomponet.component';
-import { UserpassdatacomponetComponent } from './userpassdatacomponet/userpassdatacomponet.component';
-import { ReactiveFormcomponetComponent } from './reactive-formcomponet/reactive-formcomponet.component';
-import { TemplateDrivenValidationFormcomponetComponent } from './template-driven-validation-formcomponet/template-driven-validation-formcomponet.component';
 
 export const routes: Routes = [
     
